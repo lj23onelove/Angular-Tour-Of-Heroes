d7eded7d06c9dc54ecbee1e7d4a6f03abc25cd92
lj23onelove <amirassov@alumni.nu.edu.kz>
2022-02-16 18:13:28 +0600

    Learning Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Iron Man'},
+  {id: 2, name: 'Hulk'},
+  {id: 3, name: 'Thor'},
+  {id: 4, name: 'Spider-Man'},
+  {id: 5, name: 'Wolverine'},
+  {id: 6, name: 'Hawkeye'},
+  {id: 7, name: 'Doctor Strange'},
+  {id: 8, name: 'Bucky'},
+  {id: 9, name: 'Wanda'},
+  {id: 10, name: 'Black Widow'}
+]
